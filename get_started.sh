c14e15a0a0bca2ade093ade8db8e616110c2a99e
Duncan Regan <duncan.regan@rheaply.com>
2024-01-17 16:17:56 -0500

    Update homebrew install to use bash
    This avoids the error: `Bash is required to interpret this script.`

--- get_started.sh
+++ get_started.sh
@@ -1,4 +1,4 @@
-zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 (echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zshrc
 
